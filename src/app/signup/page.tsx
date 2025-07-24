ef5447e05568a2713b7e61d9db960ad1d72a3918
Assel <asselaltayeva@users.noreply.github.com>
2025-07-24 20:16:09 +0900

    add Auth feature with JWT and password hashing

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,14 @@
+"use server"
+
+import { redirect } from "next/navigation";
+import { auth } from "~/server/auth";
+
 export default async function Page() {
+    const session = await auth();
+
+    if (session) {
+        redirect("/dashboard");
+    }
   return (
     <div>page</div>
   )
